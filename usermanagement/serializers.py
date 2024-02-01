53e1a12a3e8c13ce26f85c8a75888decedf7ceb3
David Oak <mr.davidoak@gmail.com>
2024-02-01 09:04:07 +0000

    add fnmae and lname  to  register user api endpoint

--- usermanagement/serializers.py
+++ usermanagement/serializers.py
@@ -4,12 +4,18 @@ from .models import CustomUser
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ('email', 'password')
-        extra_kwargs = {'password': {'write_only': True}}
+        fields = ('email', 'password', 'first_name', 'last_name')  
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'first_name': {'required': True},  
+            'last_name': {'required': True},   
+        }
 
     def create(self, validated_data):
         user = CustomUser.objects.create_user(
             email=validated_data['email'],
-            password=validated_data['password']
+            password=validated_data['password'],
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', '')
         )
         return user
