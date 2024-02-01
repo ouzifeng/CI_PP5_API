3ef57867d4091f144a047a8bb60f4b28b03194cf
David Oak <mr.davidoak@gmail.com>
2024-02-01 09:13:12 +0000

    fix ariable or object named status that has not been defined or imported

--- usermanagement/views.py
+++ usermanagement/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import CustomUserSerializer
-
+from rest_framework.permissions import AllowAny 
 class CustomUserCreate(APIView):
+    permission_classes = [AllowAny]
+
     def post(self, request):
         reg_serializer = CustomUserSerializer(data=request.data)
         if reg_serializer.is_valid():
