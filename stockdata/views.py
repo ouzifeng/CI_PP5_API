7cf38fe53c1e187c897bd6a708ad552b9ff7983a
David Oak <mr.davidoak@gmail.com>
2024-02-14 10:27:19 +0000

    change dividend screen pagination to 10 results from 100

--- stockdata/views.py
+++ stockdata/views.py
@@ -112,7 +112,7 @@ class StockSearchView(generics.ListAPIView):
         return queryset    
     
 class StandardResultsSetPagination(PageNumberPagination):
-    page_size = 100 
+    page_size = 10 
     page_size_query_param = 'page_size'
     max_page_size = 100    
     
