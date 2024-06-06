3fdd6172abb279101314ae40d849433600ef81f4
David Oak <mr.davidoak@gmail.com>
2024-06-06 13:23:00 +0100

    refractor management command non_dividend_stocks.py to adhere to pep8

--- stockdata/management/commands/delete_non_dividend_stocks.py
+++ stockdata/management/commands/delete_non_dividend_stocks.py
@@ -2,22 +2,29 @@ from django.core.management.base import BaseCommand
 from django.db.models import Q
 from stockdata.models import General, Highlights
 
+
 class Command(BaseCommand):
     help = 'Deletes stocks with Highlights dividend_yield blank or 0.00'
 
     def handle(self, *args, **kwargs):
         stocks_to_delete = General.objects.filter(
-            Q(highlights__dividend_yield__isnull=True) | 
+            Q(highlights__dividend_yield__isnull=True) |
             Q(highlights__dividend_yield=0.00)
         )
 
         count = stocks_to_delete.count()
 
         if count == 0:
-            self.stdout.write(self.style.SUCCESS('No stocks to delete based on dividend_yield criteria.'))
+            self.stdout.write(
+                self.style.SUCCESS(
+                    'No stocks to delete based on dividend_yield criteria.'
+                )
+            )
             return
 
-        self.stdout.write(f'Found {count} stocks with dividend_yield blank or 0.00.')
+        self.stdout.write(
+            f'Found {count} stocks with dividend_yield blank or 0.00.'
+        )
 
         confirm = input('Do you want to delete these stocks? [yes/no]: ')
 
@@ -26,7 +33,15 @@ class Command(BaseCommand):
             return
 
         for stock in stocks_to_delete.iterator():
-            self.stdout.write(f'Deleting stock: {stock.name} ({stock.code}) due to dividend_share criteria.')
+            self.stdout.write(
+                f'Deleting stock: {stock.name} ({stock.code}) due to '
+                f'dividend_share criteria.'
+            )
             stock.delete()
 
-        self.stdout.write(self.style.SUCCESS(f'Successfully deleted {count} stocks based on dividend_share criteria.'))
+        self.stdout.write(
+            self.style.SUCCESS(
+                f'Successfully deleted {count} stocks based on '
+                f'dividend_share criteria.'
+            )
+        )
