80fc2b0588343d63702c4eab673408b8d0bedb8e
David Oak <mr.davidoak@gmail.com>
2024-06-06 13:20:54 +0100

    refractor management command duplicate_stocks_delete.py to adhere to pep8

--- stockdata/management/commands/delete_duplicate_stocks_empty_sector.py
+++ stockdata/management/commands/delete_duplicate_stocks_empty_sector.py
@@ -2,23 +2,35 @@ from django.core.management.base import BaseCommand
 from django.db.models import Count
 from stockdata.models import General
 
+
 class Command(BaseCommand):
     help = 'Deletes stocks with duplicated names where sector is blank'
 
     def handle(self, *args, **kwargs):
         # Identify duplicated names
-        duplicated_names = General.objects.values('name').annotate(name_count=Count('id')).filter(name_count__gt=1)
+        duplicated_names = (
+            General.objects.values('name')
+            .annotate(name_count=Count('id'))
+            .filter(name_count__gt=1)
+        )
 
         # Filter stocks with those duplicated names where sector is blank
-        stocks_to_delete = General.objects.filter(name__in=[item['name'] for item in duplicated_names], sector__in=[None, ''])
+        stocks_to_delete = General.objects.filter(
+            name__in=[item['name'] for item in duplicated_names],
+            sector__in=[None, '']
+        )
 
         count = stocks_to_delete.count()
 
         if count == 0:
-            self.stdout.write(self.style.SUCCESS('No stocks to delete based on criteria.'))
+            self.stdout.write(
+                self.style.SUCCESS('No stocks to delete based on criteria.')
+            )
             return
 
-        self.stdout.write(f'{count} stocks found with duplicated names and blank sector.')
+        self.stdout.write(
+            f'{count} stocks found with duplicated names and blank sector.'
+        )
 
         confirm = input('Do you want to delete these stocks? [yes/no]: ')
 
@@ -27,7 +39,14 @@ class Command(BaseCommand):
             return
 
         for stock in stocks_to_delete.iterator():
-            self.stdout.write(f'Deleting stock: {stock.name} ({stock.code}) with ID: {stock.id}')
+            self.stdout.write(
+                f'Deleting stock: {stock.name} ({stock.code}) with '
+                f'ID: {stock.id}'
+            )
             stock.delete()
 
-        self.stdout.write(self.style.SUCCESS(f'Successfully deleted {count} stocks based on criteria.'))
+        self.stdout.write(
+            self.style.SUCCESS(
+                f'Successfully deleted {count} stocks based on criteria.'
+            )
+        )
