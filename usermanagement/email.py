225f912947b054ff172024542f0bde2f4ae5f6f3
David Oak <mr.davidoak@gmail.com>
2024-06-06 12:25:27 +0100

    refractor usermanagement email.py to adhere to pep8

--- usermanagement/email.py
+++ usermanagement/email.py
@@ -2,30 +2,31 @@ from django.contrib.auth.tokens import default_token_generator
 from django.utils.http import urlsafe_base64_encode
 from django.utils.encoding import force_bytes
 from django.core.mail import send_mail
-from django.urls import reverse
 from django.conf import settings
 from django.utils.html import mark_safe
-
 
 
 def send_password_reset_email(request, user):
     uid = urlsafe_base64_encode(force_bytes(user.pk))
     token = default_token_generator.make_token(user)
-      
-    # Construct the password reset URL on the frontend
-    password_reset_url = f"{settings.FRONTEND_BASE_URL}/reset-password/{uid}/{token}"
 
+    # Construct the password reset URL on the frontend
+    password_reset_url = (
+        f"{settings.FRONTEND_BASE_URL}/reset-password/{uid}/{token}"
+    )
 
     subject = "Password Reset Requested"
     message = f"""
     Hi,
 
-    You're receiving this email because you requested a password reset for your user account.
+    You're receiving this email because you requested a password reset for
+    your user account.
 
     Please go to the following link to set a new password:
     {password_reset_url}
 
-    If you did not request this, please ignore this email, and your password will remain unchanged.
+    If you did not request this, please ignore this email, and your password
+    will remain unchanged.
 
     Thanks for using our site!
     """
@@ -42,7 +43,9 @@ def send_password_reset_email(request, user):
 def send_verification_email(user, request):
     token = default_token_generator.make_token(user)
     uid = urlsafe_base64_encode(force_bytes(user.pk))
-    verification_url = f"{settings.FRONTEND_BASE_URL}verify-email/{uid}/{token}"
+    verification_url = (
+        f"{settings.FRONTEND_BASE_URL}verify-email/{uid}/{token}"
+    )
 
     subject = 'Verify your email'
     message = f"""
@@ -55,7 +58,7 @@ def send_verification_email(user, request):
 
     Thanks for using our site!
     """
-    
+
     # Mark the message as safe HTML content
     html_message = mark_safe(message)
 
