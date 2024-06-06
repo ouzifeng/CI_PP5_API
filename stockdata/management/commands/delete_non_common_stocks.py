b95479736a7edf7031b0491be99c64572681eb6a
David Oak <mr.davidoak@gmail.com>
2024-06-06 13:21:46 +0100

    refractor management command dlete_non_common_stocks.py to adhere to pep8

--- stockdata/management/commands/delete_non_common_stocks.py
+++ stockdata/management/commands/delete_non_common_stocks.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 from stockdata.models import General
 
+
 class Command(BaseCommand):
     help = 'Deletes stocks where type is not "Common Stock"'
 
@@ -10,10 +11,14 @@ class Command(BaseCommand):
         count = stocks_to_delete.count()
 
         if count == 0:
-            self.stdout.write(self.style.SUCCESS('No non-"Common Stock" types to delete.'))
+            self.stdout.write(
+                self.style.SUCCESS('No non-"Common Stock" types to delete.')
+            )
             return
 
-        self.stdout.write(f'Found {count} stocks where type is not "Common Stock".')
+        self.stdout.write(
+            f'Found {count} stocks where type is not "Common Stock".'
+        )
 
         confirm = input('Do you want to delete these stocks? [yes/no]: ')
 
@@ -22,7 +27,14 @@ class Command(BaseCommand):
             return
 
         for stock in stocks_to_delete.iterator():
-            self.stdout.write(f'Deleting stock: {stock.name} ({stock.code}) with type: {stock.type}')
+            self.stdout.write(
+                f'Deleting stock: {stock.name} ({stock.code}) with type: '
+                f'{stock.type}'
+            )
             stock.delete()
 
-        self.stdout.write(self.style.SUCCESS(f'Successfully deleted {count} non-"Common Stock" stocks.'))
+        self.stdout.write(
+            self.style.SUCCESS(
+                f'Successfully deleted {count} non-"Common Stock" stocks.'
+            )
+        )
