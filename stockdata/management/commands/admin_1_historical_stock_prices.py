063cfd21e0583077502610a606a8a54b3e04530e
David Oak <mr.davidoak@gmail.com>
2024-06-06 12:34:25 +0100

    refractor management command historical_stock_prices.py to adhere to pep8

--- stockdata/management/commands/admin_1_historical_stock_prices.py
+++ stockdata/management/commands/admin_1_historical_stock_prices.py
@@ -2,22 +2,35 @@ from django.core.management.base import BaseCommand
 from stockdata.models import General, StockPrices, SplitsDividends
 import requests
 from datetime import datetime
-import numpy as np
+from decouple import config
+
 
 class Command(BaseCommand):
-    help = 'Fetches stock prices, adjusts for stock splits, and calculates CAGR for each stock'
+    help = (
+        'Fetches stock prices, adjusts for stock splits, and calculates CAGR '
+        'for each stock'
+    )
 
     def fetch_stock_prices(self, uid):
         api_token = config('API_TOKEN')
-        url = f'https://eodhd.com/api/eod/{uid}?api_token={api_token}&fmt=json'
+        url = (
+            f'https://eodhd.com/api/eod/{uid}?api_token={api_token}&fmt=json'
+        )
         response = requests.get(url)
         if response.ok:
             try:
                 return response.json()
             except ValueError:
-                self.stdout.write(self.style.ERROR(f'Invalid JSON response for UID: {uid}'))
+                self.stdout.write(
+                    self.style.ERROR(f'Invalid JSON response for UID: {uid}')
+                )
         else:
-            self.stdout.write(self.style.ERROR(f'Failed to fetch data for UID: {uid}, HTTP Status Code: {response.status_code}'))
+            self.stdout.write(
+                self.style.ERROR(
+                    f'Failed to fetch data for UID: {uid}, HTTP Status Code: '
+                    f'{response.status_code}'
+                )
+            )
         return []
 
     def calculate_cagr(self, initial_value, final_value, years):
@@ -33,7 +46,10 @@ class Command(BaseCommand):
         for record in stock_prices:
             date = datetime.strptime(record['date'], '%Y-%m-%d').date()
             if start_year <= date.year <= current_year and date.month == 12:
-                if date.year not in end_of_year_prices or date.day > end_of_year_prices[date.year]['date'].day:
+                if (
+                    date.year not in end_of_year_prices or
+                    date.day > end_of_year_prices[date.year]['date'].day
+                ):
                     end_of_year_prices[date.year] = {
                         'date': date,
                         'price': record['close']
@@ -45,11 +61,19 @@ class Command(BaseCommand):
         adjusted_prices = []
         for year, price_data in sorted_prices:
             adjusted_price = price_data['price']
-            if splits_dividends and splits_dividends.last_split_date and splits_dividends.last_split_factor and price_data['date'] < splits_dividends.last_split_date:
-                split_factor_parts = splits_dividends.last_split_factor.split(':')
+            if (
+                splits_dividends and splits_dividends.last_split_date and
+                splits_dividends.last_split_factor and
+                price_data['date'] < splits_dividends.last_split_date
+            ):
+                split_factor_parts = splits_dividends.last_split_factor.split(
+                    ':'
+                )
                 if len(split_factor_parts) == 2:
-                    # Use float instead of int to accommodate decimal values
-                    split_factor = float(split_factor_parts[1]) / float(split_factor_parts[0])
+                    split_factor = (
+                        float(split_factor_parts[1]) /
+                        float(split_factor_parts[0])
+                    )
                     adjusted_price *= split_factor
             adjusted_prices.append(adjusted_price)
         return adjusted_prices
@@ -59,22 +83,41 @@ class Command(BaseCommand):
             # Fetch stock prices and split/dividend data
             stock_prices = self.fetch_stock_prices(general.uid)
             end_of_year_prices = self.filter_end_of_year_prices(stock_prices)
-            splits_dividends = SplitsDividends.objects.filter(general=general).first()
+            splits_dividends = SplitsDividends.objects.filter(
+                general=general
+            ).first()
 
             # Sort and adjust prices for stock splits
-            sorted_prices = sorted(end_of_year_prices.items(), key=lambda x: x[0], reverse=True)
-            adjusted_prices = self.adjust_for_splits(sorted_prices, splits_dividends)
+            sorted_prices = sorted(
+                end_of_year_prices.items(),
+                key=lambda x: x[0],
+                reverse=True
+            )
+            adjusted_prices = self.adjust_for_splits(
+                sorted_prices,
+                splits_dividends
+            )
 
             # Calculate CAGR
-            cagr_5_years = self.calculate_cagr(adjusted_prices[4], adjusted_prices[0], 5) if len(adjusted_prices) >= 5 else None
+            cagr_5_years = (
+                self.calculate_cagr(adjusted_prices[4], adjusted_prices[0], 5)
+                if len(adjusted_prices) >= 5 else None
+            )
 
             # Save the data to the database
-            stock_prices_instance, created = StockPrices.objects.update_or_create(
+            StockPrices.objects.update_or_create(
                 general=general,
-                defaults={f'Y{idx + 1}': price for idx, price in enumerate(adjusted_prices)} | {
+                defaults={
+                    f'Y{idx + 1}': price for idx, price in enumerate(
+                        adjusted_prices
+                    )
+                } | {
                     'cagr_5_years': cagr_5_years,
                 }
             )
 
-            self.stdout.write(self.style.SUCCESS(f'Successfully updated stock prices for {general.uid}'))
-
+            self.stdout.write(
+                self.style.SUCCESS(
+                    f'Successfully updated stock prices for {general.uid}'
+                )
+            )
